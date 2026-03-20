804fc5ad8d3618d3f47155e5313b37ec81dff0ad
tiantian <tiantianit@chinamobile.com>
2026-03-20 18:32:32 +0800

    update

--- alphora/web/serve.py
+++ alphora/web/serve.py
@@ -35,6 +35,12 @@ def _build_docs_tree(base: Path, rel_prefix: str = ''):
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
     def do_GET(self):
         if self.path in ('/', ''):
             self.path = '/index.html'
