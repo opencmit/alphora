cc96108a4a07bd3a38337878f1e7fd6db2f634e7
tiantian <tiantianit@chinamobile.com>
2026-04-03 11:12:23 +0800

    暂时屏蔽验证

--- alphora/__init__.py
+++ alphora/__init__.py
@@ -1,5 +1,5 @@
-from alphora._install_ext import ensure_ext_installed, ensure_license_from_env
-
-ensure_ext_installed()
-ensure_license_from_env()
+# from alphora._install_ext import ensure_ext_installed, ensure_license_from_env
+#
+# ensure_ext_installed()
+# ensure_license_from_env()
 
